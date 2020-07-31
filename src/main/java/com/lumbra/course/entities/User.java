4b7542e7eae51cc0eecfe8f29f7692e9a02634bf
Alexandre Machado <machado_silva@hotmail.com>
2020-07-31 05:02:35 -0400

    Order, Instant, ISO 8601

--- src/main/java/com/lumbra/course/entities/User.java
+++ src/main/java/com/lumbra/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.lumbra.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 
@@ -21,6 +26,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -74,6 +83,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -99,6 +112,8 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
